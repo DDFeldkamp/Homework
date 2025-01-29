c28106c40a32cbb98272296e3bef768d4dc340ac
planit <planit@tanjeffreyz.github.io>
2025-01-29 02:49:51 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,1 +1,145 @@
-assignments = {};
+assignments = {
+  "CHMENG 141": [
+    {
+      "title": "HW1",
+      "course": "CHMENG 141",
+      "dueDate": "2025-01-31T23:59:00",
+      "link": "https://www.gradescope.com/courses/956767",
+      "submitted": false
+    },
+    {
+      "title": "HW1 Bonus",
+      "course": "CHMENG 141",
+      "dueDate": "2025-01-31T23:59:00",
+      "link": "https://www.gradescope.com/courses/956767",
+      "submitted": false
+    }
+  ],
+  "CHMENG 150A": [
+    {
+      "title": "HW 1",
+      "course": "CHMENG 150A",
+      "dueDate": "2025-01-31T19:00:00",
+      "link": "https://www.gradescope.com/courses/951862",
+      "submitted": false
+    }
+  ],
+  "CS 61B": [
+    {
+      "title": "Lab 01",
+      "course": "CS 61B",
+      "dueDate": "2025-01-24T23:59:00",
+      "link": "https://www.gradescope.com/courses/961758/assignments/5640640/submissions/301967827",
+      "submitted": true
+    },
+    {
+      "title": "Homework 0A: Screenshots",
+      "course": "CS 61B",
+      "dueDate": "2025-01-24T23:59:00",
+      "link": "https://www.gradescope.com/courses/961758",
+      "submitted": false
+    },
+    {
+      "title": "Homework 0A: Code",
+      "course": "CS 61B",
+      "dueDate": "2025-01-24T23:59:00",
+      "link": "https://www.gradescope.com/courses/961758/assignments/5651868/submissions/302013665",
+      "submitted": true
+    },
+    {
+      "title": "Lab 02",
+      "course": "CS 61B",
+      "dueDate": "2025-01-31T23:59:00",
+      "link": "https://www.gradescope.com/courses/961758",
+      "submitted": false
+    },
+    {
+      "title": "Homework 0B",
+      "course": "CS 61B",
+      "dueDate": "2025-01-29T23:59:00",
+      "link": "https://www.gradescope.com/courses/961758",
+      "submitted": false
+    },
+    {
+      "title": "Project 0: 2048",
+      "course": "CS 61B",
+      "dueDate": "2025-02-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/961758",
+      "submitted": false
+    },
+    {
+      "title": "Project 0 Hard Mode: 2048",
+      "course": "CS 61B",
+      "dueDate": "2025-02-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/961758",
+      "submitted": false
+    }
+  ],
+  "CS 70": [
+    {
+      "title": "Mini-Vitamin 0A",
+      "course": "CS 70",
+      "dueDate": "2025-01-24T23:59:00",
+      "link": "https://www.gradescope.com/courses/963062/assignments/5646715/submissions/301969533",
+      "submitted": true
+    },
+    {
+      "title": "Mini-Vitamin 0B",
+      "course": "CS 70",
+      "dueDate": "2025-01-24T23:59:00",
+      "link": "https://www.gradescope.com/courses/963062/assignments/5646848/submissions/301974350",
+      "submitted": true
+    },
+    {
+      "title": "Homework 0",
+      "course": "CS 70",
+      "dueDate": "2025-01-25T16:00:00",
+      "link": "https://www.gradescope.com/courses/963062/assignments/5648304/submissions/302065230",
+      "submitted": true
+    },
+    {
+      "title": "Mini-Vitamin 1A",
+      "course": "CS 70",
+      "dueDate": "2025-01-28T18:00:00",
+      "link": "https://www.gradescope.com/courses/963062/assignments/5682042/submissions/302874033",
+      "submitted": true
+    },
+    {
+      "title": "Mini-Vitamin 1B",
+      "course": "CS 70",
+      "dueDate": "2025-01-30T18:00:00",
+      "link": "https://www.gradescope.com/courses/963062/assignments/5682046/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Homework 1",
+      "course": "CS 70",
+      "dueDate": "2025-02-01T16:00:00",
+      "link": "https://www.gradescope.com/courses/963062",
+      "submitted": false
+    }
+  ],
+  "LS 22": [
+    {
+      "title": "Syllabus Quiz  (Due Monday 1/27 2AM)",
+      "course": "LS 22",
+      "dueDate": "2025-01-27T02:00:00",
+      "link": "https://www.gradescope.com/courses/929909/assignments/5477987/submissions/302340497",
+      "submitted": true
+    },
+    {
+      "title": "Week 1 Homework (Due Monday 1/27 at 2AM)",
+      "course": "LS 22",
+      "dueDate": "2025-01-27T02:00:00",
+      "link": "https://www.gradescope.com/courses/929909/assignments/5478573/submissions/302348148",
+      "submitted": true
+    },
+    {
+      "title": "Week 2 Homework (Due Monday 2/3 at 2AM)",
+      "course": "LS 22",
+      "dueDate": "2025-02-03T02:00:00",
+      "link": "https://www.gradescope.com/courses/929909/assignments/5506692/submissions/new",
+      "submitted": false
+    }
+  ]
+};
