9d4af9fbaf60f945a24dfa8fb7228df1404ae2a5
planit <planit@tanjeffreyz.github.io>
2025-02-17 16:45:42 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -350,6 +350,13 @@ assignments = {
       "dueDate": "2025-02-17T02:00:00",
       "link": "https://www.gradescope.com/courses/929909/assignments/5724934/submissions/309078307",
       "submitted": true
+    },
+    {
+      "title": "Project 1 Assignment 3",
+      "course": "LS 22",
+      "dueDate": "2025-02-24T02:00:00",
+      "link": "https://www.gradescope.com/courses/929909",
+      "submitted": false
     }
   ]
 };
