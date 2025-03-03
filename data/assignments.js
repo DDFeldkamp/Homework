0ba74c1017059066423a0a3a929952fae139c0f3
planit <planit@tanjeffreyz.github.io>
2025-03-03 17:35:16 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -187,6 +187,13 @@ assignments = {
       "submitted": true
     },
     {
+      "title": "Lab 06",
+      "course": "CS 61B",
+      "dueDate": "2025-03-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/961758",
+      "submitted": false
+    },
+    {
       "title": "Project 2A: Ngrams",
       "course": "CS 61B",
       "dueDate": "2025-03-12T23:59:00",
