c4c924b08df431c766167044268046f8b47b60ca
planit <planit@tanjeffreyz.github.io>
2025-03-19 03:44:18 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -690,9 +690,9 @@ assignments = {
       "submitted": true
     },
     {
-      "title": "Week 9 Homework (Due Monday 3/24 at 2AM)",
+      "title": "Week 9 Homework (Due Monday 3/31 at 2AM)",
       "course": "LS 22",
-      "dueDate": "2025-03-24T02:00:00",
+      "dueDate": "2025-03-31T02:00:00",
       "link": "https://www.gradescope.com/courses/929909/assignments/5794534/submissions/new",
       "submitted": false
     },
