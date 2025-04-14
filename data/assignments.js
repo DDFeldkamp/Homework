0e98e46b0ab7d0b3cef45fbb2ce5639d654696f9
planit <planit@tanjeffreyz.github.io>
2025-04-14 05:38:46 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -875,8 +875,8 @@ assignments = {
       "title": "Week 11 Living the Topic",
       "course": "LS 22",
       "dueDate": "2025-04-14T02:00:00",
-      "link": "https://www.gradescope.com/courses/929909/assignments/6050902/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/929909/assignments/6050902/submissions/325096615",
+      "submitted": true
     }
   ]
 };
